312f52604a9b219a9522516793609da4839bff88
jmichelmarques21 <jmichelmarques21@gmail.com>
2024-06-09 00:27:20 -0300

    atualização readme

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Projeto de desenvolvimento em java - 1º Sem 2024
 |:-------------------:|:--------:|
 |   Gabriel Laurino   |    -     |
 | Guilherme Rodrigues | 36271195 |
-|        Jean         |    -     |
+|        Jean         | 33176787 |
 | João Pedro Capoani  | 32583117 |
 |   Lucas Levorato    |    -     |
 |    Luiz Padrilha    |    -     |
--- src/br/edu/up/DAO/ClienteDAO.java
+++ src/br/edu/up/DAO/ClienteDAO.java
@@ -3,32 +3,66 @@ package br.edu.up.DAO;
 import br.edu.up.Modelos.Cliente;
 import br.edu.up.Modelos.ClientePessoa;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
-public class ClienteDAO {
-  private static final String CSV_FILE = "clientes.csv";
+public class ClienteDAO{
+    public static void main(String[] args) {
+      ClienteDAO obj = new ClienteDAO();
+      obj.run();
+    }
+ public void run(){
+  String arquivoCsv = "clientes.csv";
+  BufferedReader br = null;
+  String linha = "";
+  String csvDivisor = ",";
+  try {
+    br = new BufferedReader(new FileReader(arquivoCsv));
+    while ((linha = br.readLine()) != null) {
+      String[] dados = linha.split(csvDivisor);
+      System.out.println("Dados: nome: " + dados[dados.length - 2] + ", telefone: " + dados[dados.length - 1]) + "."; 
 
-  public void adicionarCliente(Cliente cliente) throws IOException {
-    try (PrintWriter writer = new PrintWriter(new FileWriter(CSV_FILE, true))) {
-      writer.println(cliente.toString());
     }
   }
 
-  public List<Cliente> listarClientes() throws IOException {
-    List<Cliente> clientes = new ArrayList<Cliente>();
-    try (BufferedReader reader = new BufferedReader(new FileReader(CSV_FILE))){
-      String linha;
-      while ((linha = reader.readLine()) != null){
-        String[] dados = linha.split(", ");
-        String nome = dados[0].split("=")[1];
-        String cpf = dados[1].split("=")[1];
-        String telefone = dados[2].split("=")[1];
-        String endereco = dados[3].split("=")[1].replace("]", "");
-        clientes.add(new ClientePessoa(nome, telefone, cpf));
-      }
-      return clientes;
-    }
-  }
+
 }
+
+ 
+
+
+}
+
+
+// public class ClienteDAO {
+//   private static final String CSV_FILE = "clientes.csv";
+
+//   public void addCliente(Cliente cliente) throws IOException {
+//     try (CSVWriter writer = new CSVWriter(new FileWriter(CSV_FILE_PATH, true))){
+
+//     }
+//   }
+
+//   public List<Cliente> listarClientes() throws IOException {
+//     List<Cliente> clientes = new ArrayList<Cliente>();
+//     try (BufferedReader reader = new BufferedReader(new FileReader(CSV_FILE))){
+//       String linha;
+//       while ((linha = reader.readLine()) != null){
+//         String[] dados = linha.split(", ");
+//         String nome = dados[0].split("=")[1];
+//         String cpf = dados[1].split("=")[1];
+//         String telefone = dados[2].split("=")[1];
+//         String endereco = dados[3].split("=")[1].replace("]", "");
+//         clientes.add(new ClientePessoa(nome, telefone, cpf));
+//       }
+//       return clientes;
+//     }
+//   } 
+// }
