48c919155aae1d0b0b66802bfde88bf9383ee877
gabriel-laurino <gabriel_laurino@outlook.com>
2024-06-10 19:18:55 -0300

    Correção MenuView

--- src/br/edu/up/Views/MenuView.java
+++ src/br/edu/up/Views/MenuView.java
@@ -1,47 +1,66 @@
 package br.edu.up.Views;
-
-// import br.edu.up.Controller.ClienteController;
-// import br.edu.up.Controller.ContaControler;
-import br.edu.up.Controller.EnderecoController;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class MenuView {
     private Scanner scanner;
+    private ClienteView clienteView;
+    private EnderecoView enderecoView;
+    private ContaView contaView;
+    private FinanciamentoView financiamentoView;
 
-    public MenuView() {
+    public MenuView() throws IOException {
         this.scanner = new Scanner(System.in);
+        this.clienteView = new ClienteView();
+        this.enderecoView = new EnderecoView();
+        this.contaView = new ContaView();
+        this.financiamentoView = new FinanciamentoView();
     }
 
     public void exibirMenu() throws IOException {
         boolean continuar = true;
         while (continuar) {
             System.out.println("\nMenu Principal:");
-            System.out.println("1. Gerenciar Clientes");
-            System.out.println("2. Gerenciar Contas");
-            System.out.println("3. Gerenciar Endereços");
-            System.out.println("4. Sair");
+            System.out.println("1. Criar conta");
+            System.out.println("2. Buscar conta");
+            System.out.println("3. Buscar cliente");
+            System.out.println("4. Atualizar conta");
+            System.out.println("5. Adquirir financiamento");
+            System.out.println("6. Atualizar financiamento");
+            System.out.println("7. Sair");
             System.out.print("Escolha uma opção: ");
 
             int opcao = scanner.nextInt();
-            scanner.nextLine();  // Consome a nova linha
+            scanner.nextLine();
 
             switch (opcao) {
                 case 1:
-                    ClienteView clienteView = new ClienteView();
-                    // clienteView.exibirMenu();
+                    int idCliente = clienteView.AddCliente();
+                    enderecoView.adicionarEndereco(idCliente);// id do cliente criado a cima
+                    contaView.criarConta(idCliente);// id do cliente a cima
+                    // cartaoview  id da conta no cartão
                     break;
                 case 2:
-                    ContaView contaView = new ContaView();
-                    // contaView.exibirMenu();
+                   contaView.buscarConta();
                     break;
                 case 3:
-                    EnderecoView enderecoView = new EnderecoView();
-                    EnderecoController enderecoController = new EnderecoController("enderecos.csv", "cidades.csv", enderecoView);
-                    enderecoView.setController(enderecoController);
-                    enderecoView.exibirMenu();
+                clienteView.Edit();
                     break;
                 case 4:
+                    // eidtar conta que inclui editar endereço e dados do cliente ou adicionar cartão extra etc...
+                    break;
+                case 5:
+                    System.out.println("que tipo de financiamento pretende?\n");
+                    System.out.println("1 - Veicular\n2 - Imobiliario");
+                    if(scanner.nextInt()==2){
+                        financiamentoView.adicionarFinanciamentoImobiliario();
+                    }
+                    financiamentoView.adicionarFinanciamentoVeicular();
+                    break;
+                case 6:
+                    financiamentoView.menu();
+                    break;
+                case 7:
                     continuar = false;
                     break;
                 default:
